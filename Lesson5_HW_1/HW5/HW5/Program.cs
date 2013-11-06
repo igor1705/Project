816738205e372269c7ebc5c6e7a07efd1ee4437e
igor1705 <bulenko@bk.ru>
2013-11-06 14:03:22 +0200

    added lesson5

---
+++ Lesson5_HW_1/HW5/HW5/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HW5
+{
+    class Program
+    {
+
+        class coefforline
+        {
+
+            public struct coef
+            {
+                int a;
+                int b;
+            }
+
+            public static void parse (string input)
+            {
+
+            string[] parr = input.Split(';');
+                
+            }
+        }
+
+
+        public static string lineequation(int a1,int b1, int a2, int b2, int c1, int c2) 
+        {
+           /*
+           A1×X + B1×Y = C1
+           A2×X + B2×Y = C2
+           */
+
+            int newx = 0;
+            int newy = 0;
+            try
+            {
+                newx = ((c1 * b2) - (b1 * c2)) / ((a1 * b2) - (b1 * a2));
+                newy = ((a1 * c2) - (b2 * a2)) / ((a1 * b2) - (b1 * a2));
+
+                if (((a1 * newx + b1 * newy) == c1) || (a2 * newx + b2 * newy) == c2)
+                    return "Solution found: " +"\r\n"+ "x = " + newx + "\r\n" + "y = " + newy;
+                else
+                    throw new ArgumentOutOfRangeException();
+
+            }
+            catch (ArgumentOutOfRangeException e)
+            {
+                return "Arguments out of range error. Message: " + e.Message + "\r\n";
+            }
+      
+            catch (DivideByZeroException e)
+            {
+                return "Divide by zero error. Message:" + e.Message + "\r\n";
+            }
+
+     
+            
+        }
+
+
+        static void Main(string[] args)
+        {
+
+            //1
+            Console.WriteLine(Program.lineequation(2,2,4,8,0,0));
+
+            //2
+            Console.WriteLine(Program.lineequation(3, -2, 1, 3, 4, 5));
+
+            //3 - wrong solution 
+            Console.WriteLine(Program.lineequation(0, 0, 1, 3, 4, 5));
+
+            coefforline.parse("34;545;6");
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
